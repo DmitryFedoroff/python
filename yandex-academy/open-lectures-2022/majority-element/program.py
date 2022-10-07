3d1804ffb43c54e0eb14be0478e8d60ddf8c608e
DmitryFedoroff <fedoroffx@gmail.com>
2022-10-07 20:29:12 +0700

    feat: Find majority element in array

--- yandex-academy/open-lectures-2022/majority-element/program.py
+++ yandex-academy/open-lectures-2022/majority-element/program.py
@@ -1,2 +1,11 @@
+def find_majority(n, nums):
+    count = {}
+    for i in nums:
+        count[i] = count.get(i, 0) + 1
+    for val in count:
+        if count[val] > n // 2:
+            result = val
+    return result
+
 n = int(input('Enter length of array: '))
 nums = list(map(int, input('Enter numbers: ').split()))
