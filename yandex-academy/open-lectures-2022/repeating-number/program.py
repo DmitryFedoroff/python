78260f1d4fd42acc3c7df99cc2dd5cddc9658450
DmitryFedoroff <fedoroffx@gmail.com>
2022-10-10 22:06:43 +0700

    feat: Find repeating number within k distance

--- yandex-academy/open-lectures-2022/repeating-number/program.py
+++ yandex-academy/open-lectures-2022/repeating-number/program.py
@@ -1,2 +1,12 @@
+def find_rep_num(nums, k):
+    last_k = set()
+    for i in range(len(nums)):
+        if nums[i] in last_k:
+            return True
+        last_k.add(nums[i])
+        if i >= k:
+            last_k.remove(nums[i - k])
+    return False
+
 n, k = map(int, input('Enter n and k: ').split())
 nums = list(map(int, input('Enter numbers: ').split()))
