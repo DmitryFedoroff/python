ef4806c475ac1a160babca013ed5c3f436b025fb
DmitryFedoroff <fedoroffx@gmail.com>
2022-10-14 20:27:49 +0700

    feat: Count number of cows that can be arranged in stalls at largest possible allowable distance

--- yandex-academy/open-lectures-2022/cows-in-stalls/program.py
+++ yandex-academy/open-lectures-2022/cows-in-stalls/program.py
@@ -1,2 +1,11 @@
+def count_cows(distance, k, stalls):
+    counter = 1
+    prev_pos = stalls[0]
+    for i in range(1, len(stalls)):
+        if stalls[i] - prev_pos >= distance:
+            counter += 1
+            prev_pos = stalls[i]
+    return counter >= k
+
 n, k = map(int, input('Enter number of stalls and cows: ').split())
 stalls = list(map(int, input('Enter stalls coordinates: ').split()))
