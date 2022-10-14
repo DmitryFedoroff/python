acde5f5ef9704c512ba81fda3332e7b1e61dfe21
DmitryFedoroff <fedoroffx@gmail.com>
2022-10-14 20:40:21 +0700

    feat: Binary search algorithm

--- yandex-academy/open-lectures-2022/cows-in-stalls/program.py
+++ yandex-academy/open-lectures-2022/cows-in-stalls/program.py
@@ -7,5 +7,16 @@ def count_cows(distance, k, stalls):
             prev_pos = stalls[i]
     return counter >= k
 
+def binary_search(k, stalls):
+    left_bound = 0
+    right_bound = stalls[-1]
+    while right_bound > left_bound:
+        distance = (left_bound + right_bound + 1) // 2
+        if count_cows(distance, k, stalls):
+            left_bound = distance
+        else:
+            right_bound = distance - 1
+    return left_bound
+
 n, k = map(int, input('Enter number of stalls and cows: ').split())
 stalls = list(map(int, input('Enter stalls coordinates: ').split()))
