c58f02c244ad3153055cc2801626516b0165320d
DmitryFedoroff <fedoroffx@gmail.com>
2022-10-15 22:43:53 +0700

    feat: Find maximum number of groups that can be seated in classrooms

--- yandex-academy/open-lectures-2022/groups-and-classrooms/program.py
+++ yandex-academy/open-lectures-2022/groups-and-classrooms/program.py
@@ -1,3 +1,16 @@
+def seat_groups(class_cap, group_size):
+    group_size.sort()
+    class_cap.sort()
+    pos = 0
+    result = 0
+    for now in group_size:
+        while pos < len(class_cap) and class_cap[pos] < now:
+            pos += 1
+        if pos != len(class_cap):
+            result += 1
+            pos += 1
+    return result
+
 n = int(input('Enter number of classrooms: '))
 class_cap = list(map(int, input('Enter capacity of each classroom: ').split()))
 m = int(input('Enter number of groups: '))
