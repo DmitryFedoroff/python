29b0c33ac0fffdaaeb98afeafacca5959323433f
DmitryFedoroff <fedoroffx@gmail.com>
2022-10-15 21:11:25 +0700

    dc: Initialize variables and list

---
+++ yandex-academy/open-lectures-2022/groups-and-classrooms/program.py
@@ -0,0 +1,4 @@
+n = int(input('Enter number of classrooms: '))
+class_cap = list(map(int, input('Enter capacity of each classroom: ').split()))
+m = int(input('Enter number of groups: '))
+group_size = list(map(int, input('Enter number of students in each group: ').split()))
