4a0c208e947384fa63a7440902b9b8c89204e452
DmitryFedoroff <fedoroffx@gmail.com>
2022-10-18 21:06:23 +0700

    feat: Find total number of invitations

--- yandex-academy/open-lectures-2022/boomers-and-zoomers/program.py
+++ yandex-academy/open-lectures-2022/boomers-and-zoomers/program.py
@@ -1,2 +1,14 @@
+def find_number_invitations(n, age):
+    age.sort()
+    left, right, result = 0, 0, 0
+    for i in range(n):
+        while (left < n) and (age[left] <= 0.5 * age[i] + 7):
+            left += 1
+        while (right < n) and (age[right] <= age[i]):
+            right += 1
+        if right > left + 1:
+            result += right - left - 1
+    return result
+
 n = int(input('Number of people: '))
 age = list(map(int, input('Age of people: ').split()))
