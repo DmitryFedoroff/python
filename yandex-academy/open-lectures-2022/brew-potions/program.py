df6d7fd904737d090aa297b0a6ea1c4dfb376cb9
DmitryFedoroff <fedoroffx@gmail.com>
2022-10-21 23:47:17 +0700

    feat: Calculate maximum total utility of potions

--- yandex-academy/open-lectures-2022/brew-potions/program.py
+++ yandex-academy/open-lectures-2022/brew-potions/program.py
@@ -1,6 +1,35 @@
 INF = int(1e18)
 MAX_ELEM = int(1e9)
 
+def calc_util(min_util):
+    result = 0
+    count = 0
+    for i in range(1, n + 1):
+        if utility[i] < min_util:
+            break
+        count += 1
+        result += utility[i]
+    
+    j = 2
+    while (j <= n) and (utility[1] + utility[j] >= min_util):
+        j += 1
+
+    pref_sum = [0] * (n + 1)
+    for i in range(1, n + 1):
+        pref_sum[i] = pref_sum[i - 1] + utility[i]
+
+    for i in range(1, n + 1):
+        if i + 1 >= j:
+            break
+        while (j - 1 > i) and (utility[i] + utility[j - 1] < min_util):
+            j -= 1
+        count += (j - i - 1)
+        result += pref_sum[j - 1] - pref_sum[i] + utility[i] * (j - i -1)
+    if count >= k:
+        return result - (count - k) * min_util
+    else:
+        return INF
+
 def binary_search():
     utility.sort()
     utility.append(0)
