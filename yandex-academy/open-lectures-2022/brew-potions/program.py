91665a13d915a367835945aae16ec27b56cd6e3b
DmitryFedoroff <fedoroffx@gmail.com>
2022-10-21 23:39:48 +0700

    feat: Search for utility of weakest potion from best available potions

--- yandex-academy/open-lectures-2022/brew-potions/program.py
+++ yandex-academy/open-lectures-2022/brew-potions/program.py
@@ -1,5 +1,19 @@
 INF = int(1e18)
 MAX_ELEM = int(1e9)
 
+def binary_search():
+    utility.sort()
+    utility.append(0)
+    utility.reverse()
+    l = -MAX_ELEM
+    r = MAX_ELEM
+    while r > l:
+        m = (l + r + 1) // 2
+        if calc_util(m) != INF:
+            l = m
+        else:
+            r = m - 1
+    return l
+
 n, k = map(int, input().split())
 utility = list(map(int, input().split()))
