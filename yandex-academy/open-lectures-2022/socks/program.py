5408fa991353781c7867e6802acd54e1c36ca5e8
DmitryFedoroff <fedoroffx@gmail.com>
2022-10-26 20:38:43 +0700

    feat: Read content of text file

--- yandex-academy/open-lectures-2022/socks/program.py
+++ yandex-academy/open-lectures-2022/socks/program.py
@@ -1,1 +1,14 @@
+import os.path
+
+def read_file(file):
+    if not os.path.isfile(file):
+        print(f'File {file} does not exist')
+    elif os.path.getsize(file) == 0:
+        print(f'File {file} is empty')
+    else:
+        with open(file, 'r', encoding='utf-8') as f:
+            contents = f.read().split('\n')
+            print(f'File "{file}" read successfully')
+        return contents
+
 l, n, m = map(int, input('Enter L M N numbers: ').split())
