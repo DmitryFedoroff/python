738ee90dc8d71a0335b7b8fde266c90c93bcdc7f
DmitryFedoroff <fedoroffx@gmail.com>
2022-10-28 21:21:48 +0700

    feat: Count maximum number of applications can be fulfilled

--- yandex-academy/open-lectures-2022/applications-selection/program.py
+++ yandex-academy/open-lectures-2022/applications-selection/program.py
@@ -1,1 +1,15 @@
+def find_max_applic(n):
+    applic = []
+    for i in range(n):
+        s = list(map(int, input('Enter number si and fi: ').split()))
+        applic.append(s)
+    count = 1
+    applic.sort(key = lambda x: x[1])
+    end = applic[0][1]
+    for i in range(1, n):
+        if applic[i][0] >= end:
+            count += 1
+            end = applic[i][1]
+    return count
+
 n = int(input('Enter number of applications: '))
