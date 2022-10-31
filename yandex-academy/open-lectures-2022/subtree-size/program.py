b87b935e1771573dec64488c4a3891f1b5573876
DmitryFedoroff <fedoroffx@gmail.com>
2022-10-31 20:43:21 +0700

    feat: Search tree data using depth-first search algorithm

--- yandex-academy/open-lectures-2022/subtree-size/program.py
+++ yandex-academy/open-lectures-2022/subtree-size/program.py
@@ -2,4 +2,11 @@ import sys
 
 sys.setrecursionlimit(100000)
 
+def dfs(now, neighbours, subtree_size):
+    subtree_size[now] = 1
+    for next in neighbours[now]:
+        if subtree_size[next] == -1:
+            subtree_size[now] += dfs(next, neighbours, subtree_size)
+    return subtree_size[now]
+
 n = int(input('Number of vertices: '))
