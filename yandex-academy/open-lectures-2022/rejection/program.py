d2d7845fde0b14b9546ccdd12b8184fc9abb4101
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-03 20:36:08 +0700

    feat: Check spelling of substances

--- yandex-academy/open-lectures-2022/rejection/program.py
+++ yandex-academy/open-lectures-2022/rejection/program.py
@@ -3,6 +3,29 @@ def unvovel(s):
      for vovel in 'aeuio':
          s = s.replace(vovel, '#')
      return s
+     
+def check_spelling(wordlist, queries):
+    dct = set()
+    dct_lower = {}
+    dct_unvovel = {}
+    for word in wordlist:
+        dct.add(word)
+        if word.lower() not in dct_lower:
+            dct_lower[word.lower()] = word
+        unvoveled = unvovel(word)
+        if unvoveled not in dct_unvovel:
+            dct_unvovel[unvoveled] = word
+    result = []
+    for word in queries:
+        if word in dct:
+            result.append(word)
+        elif word.lower() in dct_lower:
+            result.append(dct_lower[word.lower()])
+        elif unvovel(word) in dct_unvovel:
+            result.append(dct_unvovel[unvovel(word)])
+        else:
+            result.append('')
+    return result
 
 dct_len = int(input('Enter number of substances in list of desirable substances: '))
 dct = list(input('Enter list of desirable substances: ').split())
