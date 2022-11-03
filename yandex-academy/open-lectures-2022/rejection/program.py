6bacfc3d7a205671fa4c1892d9177c8d0ae3c93f
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-03 19:55:23 +0700

    dc: Initialize variables and lists

---
+++ yandex-academy/open-lectures-2022/rejection/program.py
@@ -0,0 +1,4 @@
+dct_len = int(input('Enter number of substances in list of desirable substances: '))
+dct = list(input('Enter list of desirable substances: ').split())
+txt_len = int(input('Enter number of substances obtained in the reactor: '))
+txt = list(input('Enter list of substances obtained in the reactor: ').split())
