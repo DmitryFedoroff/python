9dd4da5dc73dd8cedb1a7e46acd5eb036a4ea497
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-04 23:34:33 +0700

    feat: Check if input is integer and raise error if desired value is not integer

--- geekbrains/weekend-or-not/program.py
+++ geekbrains/weekend-or-not/program.py
@@ -1,2 +1,12 @@
+def get_int_input(value):
+    while True:
+        try:
+            s = input(f"{value}")
+            return int(s)
+        except ValueError:
+            print("Input value is not integer. Please try again ...")
+
+
+
 if __name__ == '__main__':
     day_num = get_int_input("Enter day number: ")
