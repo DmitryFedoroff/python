5e15aa02539c844adeffbdbdbbca260f60e466a2
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-04 23:40:22 +0700

    feat: Check whether day number is weekend or weekday

--- geekbrains/weekend-or-not/program.py
+++ geekbrains/weekend-or-not/program.py
@@ -7,6 +7,14 @@ def get_int_input(value):
             print("Input value is not integer. Please try again ...")
 
 
+def check_day(num):
+    if 6 <= num <= 7:
+        print("Yes, it's weekend")
+    elif 0 < num < 6:
+        print("No, it's weekday")
+    else:
+        print("Number is outside 7-day week")
+
 
 if __name__ == '__main__':
     day_num = get_int_input("Enter day number: ")
