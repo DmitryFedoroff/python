444be744730a2e52e830420a497d63f4c9f2c4dd
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-07 23:04:33 +0700

    feat: Check if input is integer and raise error if desired value is not integer

--- geekbrains/largest-number-2d-list/program.py
+++ geekbrains/largest-number-2d-list/program.py
@@ -1,10 +1,18 @@
 from random import *
 
 
+def get_int_input(value):
+    while True:
+        try:
+            s = input(value)
+            return int(s)
+        except ValueError:
+            print("Input value is not integer. Please try again ...")
+
+
 if __name__ == '__main__':
     row = get_int_input("Enter number of rows: ")
     col = get_int_input("Enter number of columns: ")
     start = get_int_input("Enter lower limit of range: ")
     end = get_int_input("Enter upper limit of range: ")
     list_2d = [[randint(start, end) for x in range(col)] for y in range(row)]
-    
