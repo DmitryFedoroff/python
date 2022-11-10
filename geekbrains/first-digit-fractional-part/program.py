1bf7a17cdcfd14ce0609ab58cbf64161b1423d43
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-10 21:12:56 +0700

    refactor: Remove redundant value variable

--- geekbrains/first-digit-fractional-part/program.py
+++ geekbrains/first-digit-fractional-part/program.py
@@ -1,8 +1,8 @@
 import re
 
 
-def get_float_input(value):
-    s = input(value)
+def get_float_input():
+    s = input()
     if re.match(r'[+-]?[0-9]+\.[0-9]+', s) is None:
         raise ValueError("Input is not floating point number. Please try again ...")
     return float(s)
