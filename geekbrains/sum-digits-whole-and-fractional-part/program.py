cdf8f159ba0cdfe01b5e101de4cf6fe7d888ab34
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-11 22:10:35 +0700

    feat: Check if input is float and raise error if desired value is not float

--- geekbrains/sum-digits-whole-and-fractional-part/program.py
+++ geekbrains/sum-digits-whole-and-fractional-part/program.py
@@ -1,2 +1,12 @@
+import re
+
+
+def get_float_input():
+    s = input()
+    if re.match(r'[+-]?[0-9]+\.[0-9]+', s) is None:
+        raise ValueError("Input is not floating point number. Please try again ...")
+    return float(s)
+
+
 if __name__ == '__main__':
     n = get_float_input()
