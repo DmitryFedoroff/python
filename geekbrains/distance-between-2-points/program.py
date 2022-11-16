5c0cb45fe9e9a8c0564b0bf187e2965d7e18d9fc
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-16 21:45:42 +0700

    feat: Check if input is integer and raise error if desired value is not integer

--- geekbrains/distance-between-2-points/program.py
+++ geekbrains/distance-between-2-points/program.py
@@ -1,3 +1,11 @@
+def get_int_input():
+    while True:
+        try:
+            return list(map(int, input().split()))
+        except ValueError:
+            print("Input value is not integer. Please try again ...")
+
+
 if __name__ == '__main__':
     a = get_int_input()
     b = get_int_input()
