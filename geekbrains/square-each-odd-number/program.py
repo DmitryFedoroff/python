9c428e1876d757282907dfdd0314d8e8174b0ad3
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-18 23:22:22 +0700

    feat: Make square of each odd number in list and print result

--- geekbrains/square-each-odd-number/program.py
+++ geekbrains/square-each-odd-number/program.py
@@ -7,6 +7,10 @@ def get_int_input():
             print("Input value is not integer. Please try again ...")
 
 
+def square_odd_nums(ls):
+    [print(x ** 2, end='\t') for x in ls if x % 2 > 0]
+
+
 if __name__ == '__main__':
     n = get_int_input()
     lst = list(range(n))
