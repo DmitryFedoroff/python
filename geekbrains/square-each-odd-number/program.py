cf5722ce5d0a0fee067f1c5f411383f7b3a5a15a
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-18 23:18:27 +0700

    feat: Check if input is integer and raise error if desired value is not integer

--- geekbrains/square-each-odd-number/program.py
+++ geekbrains/square-each-odd-number/program.py
@@ -1,3 +1,12 @@
+def get_int_input():
+    while True:
+        try:
+            s = input()
+            return int(s)
+        except ValueError:
+            print("Input value is not integer. Please try again ...")
+
+
 if __name__ == '__main__':
     n = get_int_input()
     lst = list(range(n))
