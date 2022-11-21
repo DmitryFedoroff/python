db1e09f6f87e72a9b117f568bc61fd717c79ebf1
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-21 23:17:30 +0700

    feat: Check if input is numerical and raise error if desired value is string

--- geekbrains/quadrants-of-coordinates/program.py
+++ geekbrains/quadrants-of-coordinates/program.py
@@ -1,3 +1,13 @@
+import re
+
+
+def get_num_input():
+    s = input()
+    if re.match('^-?\\d*(\\.\\d+)?$', s) is None:
+        raise ValueError("Input is not numerical. Please try again ...")
+    return float(s)
+
+
 if __name__ == '__main__':
     px = get_num_input()
     py = get_num_input()
