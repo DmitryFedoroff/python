7104abcf7f6aa6434015636c6cc2d194364c0608
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-22 22:34:44 +0700

    feat: Check if input is integer and raise error if desired value is not integer

--- geekbrains/sum-sequence-of-numbers/program.py
+++ geekbrains/sum-sequence-of-numbers/program.py
@@ -1,2 +1,11 @@
+def get_int_input():
+    while True:
+        try:
+            s = input()
+            return int(s)
+        except ValueError:
+            print("Input value is not integer. Please try again ...")
+
+
 if __name__ == '__main__':
     num = get_int_input()
