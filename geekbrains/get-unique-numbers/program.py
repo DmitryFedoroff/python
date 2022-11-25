d573ed68f62a08c46505b150dd615ec025436b0f
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-25 23:34:30 +0700

    feat: Create sorted list of unique numbers from two-dimensional list of integers

--- geekbrains/get-unique-numbers/program.py
+++ geekbrains/get-unique-numbers/program.py
@@ -13,8 +13,13 @@ def print_list(lst):
     [print(*x, sep='\t') for x in lst]
 
 
+def get_unique_nums(lst):
+    return sorted(set(i for j in lst for i in j))
+
+
 if __name__ == '__main__':
     row, col = get_int_input()
     start, end = get_int_input()
     list_2d = [[randint(start, end) for x in range(col)] for y in range(row)]
     print_list(list_2d)
+    print(get_unique_nums(list_2d))
