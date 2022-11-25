8c630d008fd074e5fa9f5bb8b838a932ab87f5dd
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-25 23:30:31 +0700

    dc: Add print_list function for two-dimensional list

--- geekbrains/get-unique-numbers/program.py
+++ geekbrains/get-unique-numbers/program.py
@@ -17,3 +17,4 @@ if __name__ == '__main__':
     row, col = get_int_input()
     start, end = get_int_input()
     list_2d = [[randint(start, end) for x in range(col)] for y in range(row)]
+    print_list(list_2d)
