f6ac936cbb208549178b63311da5380fc5dcdd00
DmitryFedoroff <fedoroffx@gmail.com>
2022-11-30 22:35:18 +0700

    feat: Make increasing number sequence consisting of elements of original list of integers

--- geekbrains/make-increasing-sequence/program.py
+++ geekbrains/make-increasing-sequence/program.py
@@ -10,6 +10,12 @@ def get_int_input():
             print("Input value is not integer. Please try again ...")
 
 
+def make_incr_seq(ls):
+    seq = []
+    [seq.append(ls[i]) for i in range(len(ls)) if ls[i] == max(ls[:i + 1:]) and ls[i] not in seq]
+    return seq
+
+
 if __name__ == '__main__':
     n = get_int_input()
     lst = [random.randint(1, 10) for i in range(n)]
