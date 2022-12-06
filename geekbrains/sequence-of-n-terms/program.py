ad9645dcfdd43f10c37797fb991451829a3ff13b
DmitryFedoroff <fedoroffx@gmail.com>
2022-12-06 21:31:04 +0700

    feat: Check whether input value is integer

--- geekbrains/sequence-of-n-terms/program.py
+++ geekbrains/sequence-of-n-terms/program.py
@@ -1,3 +1,12 @@
+def get_int_input():
+    while True:
+        try:
+            s = input()
+            return int(s)
+        except ValueError:
+            print("Input value is not integer. Please try again ...")
+
+
 if __name__ == '__main__':
     num = get_int_input()
 
