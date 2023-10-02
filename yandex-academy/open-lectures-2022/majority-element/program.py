9630f094ec843cc508f218ef858c05b35d43856c
DmitryFedoroff <fedoroffx@gmail.com>
2023-10-02 19:58:08 +0700

    refactor: Enhance code structure and readability
    #comment Refactor the find_majority function to improve its logic and readability by simplifying the loop structure and removing unnecessary variables. Also, enhance user input handling by removing the requirement of entering the length of the array, making the input process more user-friendly and the code cleaner
    
    Affected: find_majority, Main (user input handling, majority finding)

--- yandex-academy/open-lectures-2022/majority-element/program.py
+++ yandex-academy/open-lectures-2022/majority-element/program.py
@@ -1,12 +1,12 @@
-def find_majority(n, nums):
-    count = {}
-    for i in nums:
-        count[i] = count.get(i, 0) + 1
-    for val in count:
-        if count[val] > n // 2:
-            result = val
-    return result
+def find_majority(nums):
+    candidate = count = 0
+    for num in nums:
+        if count == 0: candidate = num
+        count += 1 if num == candidate else -1
+    return candidate if nums.count(candidate) > len(nums) // 2 else 'No majority element'
 
-n = int(input('Enter length of array: '))
-nums = list(map(int, input('Enter numbers: ').split()))
-print(f'Majority element in array: {find_majority(n, nums)}')
+
+if __name__ == '__main__':
+    _ = input('Enter length of array: ')
+    nums = list(map(int, input('Enter numbers: ').split()))
+    print(f'Majority element in array: {find_majority(nums)}')
