02f2a76738db5ad7549e43d9e1439a62f69d0497
DmitryFedoroff <fedoroffx@gmail.com>
2023-10-04 20:28:00 +0700

    refactor: Enhance code structure and readability
    #comment Streamlined the find_min_dist function by leveraging list comprehensions and eliminating redundant variables. Introduced the __name__ == '__main__' guard to ensure modularity and better encapsulation of the main execution logic. Sorted the time points directly during the conversion to minutes, reducing the need for additional sorting calls
    
    Affected: find_min_dist function, Main execution block

--- yandex-academy/open-lectures-2022/minimum-time-difference/program.py
+++ yandex-academy/open-lectures-2022/minimum-time-difference/program.py
@@ -1,15 +1,10 @@
-def find_min_dist(n, time_points):
-    day = 1440
-    minute_points = []
-    for now_time in time_points:
-        h, m = map(int, now_time.split(':'))
-        minute_points.append(h * 60 + m)
-    minute_points.sort()
-    min_dist = day + minute_points[0] - minute_points[-1]
-    for i in range(1, len(minute_points)):
-        min_dist = min(min_dist, minute_points[i] - minute_points[i - 1])
-    return min_dist
+def find_min_dist(time_points):
+    minute_points = sorted(int(h) * 60 + int(m) for h, m in (tp.split(':') for tp in time_points))
+    min_dists = min(minute_points[i] - minute_points[i - 1] for i in range(1, len(minute_points)))
+    return min(min_dists, 1440 + minute_points[0] - minute_points[-1])
 
-n = int(input('Enter number of trains: '))
-time_points = input('Enter 24-hour clock time points (HH:MM): ').split()
-print(f'Minimum minutes difference between arrival of two trains: {find_min_dist(n, time_points)}')
+
+if __name__ == '__main__':
+    input('Enter number of trains: ')
+    time_points = input('Enter 24-hour clock time points (HH:MM): ').split()
+    print(f'Minimum minutes difference between arrival of two trains: {find_min_dist(time_points)}')
