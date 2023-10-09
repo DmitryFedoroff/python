f98c129fa18e836cb2a7d97524217763b6882c4d
DmitryFedoroff <fedoroffx@gmail.com>
2023-10-09 19:01:36 +0700

    refactor: Enhance code structure and readability
    #comment Replaced lambda function with a defined function for better readability. Enhanced anagram check logic to not just compare sorted strings, but also check the count of each character for improved accuracy. Encapsulated main execution within a main function and added a check for __name__ == '__main__' for more structured code execution
    
    Affected: is_anagram (anagram check), main (program execution), Main (__name__ check)

--- yandex-academy/open-lectures-2022/valid-anagram/program.py
+++ yandex-academy/open-lectures-2022/valid-anagram/program.py
@@ -1,9 +1,13 @@
-is_anagram = lambda s, t: sorted(s) == sorted(t)
+def is_anagram(string1, string2):
+    return set(string1) == set(string2) and all(string1.count(char) == string2.count(char) for char in set(string1))
 
-s = input('Enter 1st string: ')
-t = input('Enter 2nd string: ')
 
-if is_anagram:
-    print('Yes')
-else:
-    print('No')
+def main():
+    str1 = input('Enter 1st string: ')
+    str2 = input('Enter 2nd string: ')
+    message = 'Yes' if is_anagram(str1, str2) else 'No'
+    print(message)
+
+
+if __name__ == '__main__':
+    main()
