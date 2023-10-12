65e833aad62c16ca9f5254dd06f58ddac776f7c4
DmitryFedoroff <fedoroffx@gmail.com>
2023-10-12 16:59:56 +0700

    refactor: Enhance code structure and readability
    #comment Refactor the find_path function to improve its structure and readability. Introduce a variable 'entry_with_spaces' to capture user input with leading spaces, and another variable 'num_spaces' to store the count of leading spaces. This modularizes the space-counting logic and improves its readability. Also, rename variables to more semantically meaningful names like 'target_file' and 'num_entries' for increased code clarity. The main execution block is adjusted to handle the input process and call the find_path function, adhering to best practices for separating business logic from execution
    
    Affected: find_path function, Main (file and directory path finding)

--- yandex-academy/open-lectures-2022/path-to-file/program.py
+++ yandex-academy/open-lectures-2022/path-to-file/program.py
@@ -1,16 +1,17 @@
-def find_path(query, n):
-    now_path = []
-    for i in range(n):
-        line = input('Enter <␣␣filename.*> or <␣directory> name: ')
-        now_file = line.strip()
-        spaces = 0
-        while (spaces < len(line)) and (line[spaces] == ' '):
-            spaces += 1
-        now_path = now_path[:spaces]
-        now_path.append(now_file)
-        if now_file == query:
-            return '/' + '/'.join(now_path)
+def find_path(target_file, num_entries):
+    current_path = []
+    for _ in range(num_entries):
+        entry_with_spaces = input('Enter < filename.* > or < directory > name: ')
+        num_spaces = len(entry_with_spaces) - len(entry_with_spaces.lstrip())
+        entry = entry_with_spaces.strip()
+        current_path = current_path[:num_spaces]
+        current_path.append(entry)
+        if entry == target_file:
+            return '/' + '/'.join(current_path)
 
-query = input('Enter file name: ')
-n = int(input('Enter total number of files and directories: '))
-print(f'Path to file: {find_path(query, n)}')
+
+if __name__ == '__main__':
+    query_file = input('Enter file name: ')
+    total_entries = int(input('Enter total number of files and directories: '))
+    result = find_path(query_file, total_entries)
+    print(f'Path to file: {result}')
