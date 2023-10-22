e954228d689f57eb2c1054750e1d66286f761eda
DmitryFedoroff <fedoroffx@gmail.com>
2023-10-22 23:36:47 +0700

    refactor: Enhance code structure and readability
    #comment Introduced type hints for better code comprehension. Refined input validation using regex pattern matching. Also, switched to a while loop for continuous input validation until a correct float number is entered. Changed function names to be more descriptive and meaningful
    
    Affected: get_float_input, get_fractional_digit, Main (code structure and readability)

--- geekbrains/first-digit-fractional-part/program.py
+++ geekbrains/first-digit-fractional-part/program.py
@@ -1,16 +1,19 @@
 import re
 
 
-def get_float_input():
-    s = input()
-    if re.match(r'[+-]?[0-9]+\.[0-9]+', s) is None:
-        raise ValueError("Input is not floating point number. Please try again ...")
-    return float(s)
+def get_float_input(prompt: str = "Enter a float number: ") -> float:
+    float_pattern = re.compile(r'[+-]?[0-9]+\.[0-9]+')
+    while True:
+        user_input = input(prompt).strip()
+        if float_pattern.fullmatch(user_input):
+            return float(user_input)
+        print("Invalid input. Please enter a valid floating-point number.")
 
 
-def get_fract_digit(fnum): return int(abs(fnum * 10) % 10)
+def get_fractional_digit(float_num: float) -> int:
+    return int(abs(float_num * 10) % 10)
 
 
 if __name__ == '__main__':
-    n = get_float_input()
-    print(get_fract_digit(n))
+    num = get_float_input()
+    print(get_fractional_digit(num))
