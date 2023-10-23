3148630d3a4858ce523bf615994deb7ab5c253ae
DmitryFedoroff <fedoroffx@gmail.com>
2023-10-23 19:39:12 +0700

    refactor: Enhance code structure and readability
    #comment The changes include more descriptive function names, splitting the get_int_input function into get_dimensions and get_range for better separation of concerns, and using map to simplify code. Error handling has also been improved for user input. The print_list function has been renamed to print_2d_list and now uses the join method for better performance. The find_max function has been renamed to find_largest and now uses a more Pythonic approach to find the maximum value
    
    Affected: get_integer_input, get_dimensions, get_range, generate_2d_list, print_2d_list, find_largest, Main (overall code structure and readability)

--- geekbrains/largest-number-2d-list/program.py
+++ geekbrains/largest-number-2d-list/program.py
@@ -1,23 +1,40 @@
-from random import *
+from random import randint
 
 
-def get_int_input():
+def get_integer_input(prompt="Enter an integer: ") -> int:
     while True:
+        user_input = input(prompt).strip()
         try:
-            return list(map(int, input().split()))
+            return int(user_input)
         except ValueError:
-            print("Input value is not integer. Please try again ...")
+            print("Invalid input. Please enter a valid integer.")
 
 
-def print_list(lst): [print(*x, sep='\t') for x in lst]
+def get_dimensions():
+    return map(get_integer_input, ["Enter number of rows: ", "Enter number of columns: "])
 
 
-def find_max(lst): return max(max(x) for x in lst)
+def get_range():
+    return map(get_integer_input, ["Enter lower limit: ", "Enter upper limit: "])
+
+
+def generate_2d_list(rows, cols, start, end):
+    return [[randint(start, end) for _ in range(cols)] for _ in range(rows)]
+
+
+def print_2d_list(matrix):
+    for row in matrix:
+        print('\t'.join(map(str, row)))
+
+
+def find_largest(matrix):
+    return max(max(row) for row in matrix)
 
 
 if __name__ == '__main__':
-    row, col = get_int_input()
-    start, end = get_int_input()
-    list_2d = [[randint(start, end) for x in range(col)] for y in range(row)]
-    print_list(list_2d)
-    print(find_max(list_2d))
+    rows, cols = get_dimensions()
+    start, end = get_range()
+    matrix = generate_2d_list(rows, cols, start, end)
+    print_2d_list(matrix)
+    largest_number = find_largest(matrix)
+    print(f"The largest number in the generated 2D list is: {largest_number}")
