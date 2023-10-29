b1a32d68d134b3245929fb9b89d9168fc62d9ffe
DmitryFedoroff <fedoroffx@gmail.com>
2023-10-29 21:30:58 +0700

    refactor: Enhance code structure and readability
    #comment Refactor the methods get_integer_input and generate_sequence to improve code readability and structure. Type hinting has been added to both methods to make the expected types clear. The get_integer_input method now takes an optional prompt argument for better reusability. Both methods have been renamed to better describe their functionality
    
    Affected: get_integer_input, generate_sequence, Main (sequence generation and input handling)

--- geekbrains/sequence-of-n-terms/program.py
+++ geekbrains/sequence-of-n-terms/program.py
@@ -1,16 +1,16 @@
-def get_int_input():
+def get_integer_input(prompt="Enter an integer: ") -> int:
     while True:
+        user_input = input(prompt).strip()
         try:
-            s = input()
-            return int(s)
+            return int(user_input)
         except ValueError:
-            print("Input value is not integer. Please try again ...")
+            print("Invalid input. Please enter a valid integer.")
 
 
-def get_seq(n):
-    return [((-3) ** x) for x in range(n)]
+def generate_sequence(length: int) -> list:
+    return [(-3) ** i for i in range(length)]
 
 
 if __name__ == '__main__':
-    num = get_int_input()
-    print(*get_seq(num))
+    num_elements = get_integer_input("Enter the length of the sequence: ")
+    print(*generate_sequence(num_elements))
