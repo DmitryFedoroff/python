a2b3175a44e59307b29c9fe6dbe9c291cc3812b7
DmitryFedoroff <fedoroffx@gmail.com>
2024-01-02 00:06:26 +0700

    feat: Read list of integers from user
    #comment Implement read_integer_list function to prompt the user for a list of integers and return it as a list. Includes input validation to ensure only valid integers are entered
    
    Affected: read_integer_list

--- yandex-academy/open-lectures-2022/positive-numbers-in-segment/program.py
+++ yandex-academy/open-lectures-2022/positive-numbers-in-segment/program.py
@@ -5,3 +5,11 @@ def read_integer(prompt='Enter an integer: ') -> int:
             return int(user_input)
         except ValueError:
             print('Invalid input. Please enter a valid integer.')
+
+
+def read_integer_list(prompt='Enter a list of integers: '):
+    while True:
+        try:
+            return list(map(int, input(prompt).split()))
+        except ValueError:
+            print('Invalid input. Please enter a list of valid integers.')
