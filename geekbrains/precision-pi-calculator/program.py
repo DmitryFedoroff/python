300228feaf5800fda722e74703ce44848337cb7c
DmitryFedoroff <fedoroffx@gmail.com>
2024-01-06 22:11:27 +0700

    feat: Calculate Pi to specified precision
    #comment Add calculate_pi_to_precision function for estimating the value of Pi to a given precision. Utilizes an iterative approach for the calculation based on the Leibniz formula for Pi
    
    Affected: calculate_pi_to_precision

--- geekbrains/precision-pi-calculator/program.py
+++ geekbrains/precision-pi-calculator/program.py
@@ -8,3 +8,15 @@ def get_float_input(prompt: str = "Enter the precision (e.g., 0.001): ") -> floa
         if float_pattern.fullmatch(user_input):
             return float(user_input)
         print("Invalid input. Please enter a valid precision (e.g., 0.001).")
+
+
+def calculate_pi_to_precision(precision):
+    pi_estimate = 0
+    k = 1
+    for i in range(int(1 / precision)**2):
+        if i % 2 == 0:
+            pi_estimate += 4 / k
+        else:
+            pi_estimate -= 4 / k
+        k += 2
+    return pi_estimate
