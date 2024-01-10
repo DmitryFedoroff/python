936e13bd67a58870a6b8b0fb8a92fd54b6c6001a
DmitryFedoroff <fedoroffx@gmail.com>
2024-01-11 01:30:23 +0700

    feat: Execute main application flow
    #comment Set up the main execution flow in main.py. Initialize and start PhonebookController to manage the application
    
    Affected: main.py (main execution block)

---
+++ geekbrains/phonebook/main.py
@@ -0,0 +1,10 @@
+from controller.phonebook_controller import PhonebookController
+
+
+def main():
+    controller = PhonebookController()
+    controller.start()
+
+
+if __name__ == '__main__':
+    main()
