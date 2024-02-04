ca9667a1301744a5561659436c77a443b9afb1fc
DmitryFedoroff <fedoroffx@gmail.com>
2024-02-04 13:04:04 +0700

    refactor: Integrate type hints into logger configuration
    #comment Introduce type annotations in the get_logger function to define explicit types for the input parameter and return value. Facilitates better integration with IDEs and static analysis tools, enhancing code quality
    
    Affected: get_logger

--- geekbrains/phonebook/utils/logger_config.py
+++ geekbrains/phonebook/utils/logger_config.py
@@ -1,14 +1,14 @@
 import logging
 
 
-def get_logger(name):
+def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     log_format = '%(asctime)s - %(levelname)s - %(name)s : %(message)s'
     log_file_name = 'phonebook.log'
 
-    if not any(isinstance(handler, logging.FileHandler) and handler.baseFilename.endswith(log_file_name) for handler in logger.handlers):
-        file_handler = logging.FileHandler(log_file_name)
+    if not any(isinstance(handler, logging.FileHandler) and handler.baseFilename == log_file_name for handler in logger.handlers):
+        file_handler = logging.FileHandler(log_file_name, encoding='utf-8')
         formatter = logging.Formatter(log_format)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
