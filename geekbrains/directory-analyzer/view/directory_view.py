1f6990566d40070356ccc6bcb33f9c30cd4463c1
DmitryFedoroff <fedoroffx@gmail.com>
2024-04-04 19:58:26 +0700

    feat: Output user messages
    #comment Provide a method for displaying user messages to enhance user interaction and feedback
    
    Affected: DirectoryView

--- geekbrains/directory-analyzer/view/directory_view.py
+++ geekbrains/directory-analyzer/view/directory_view.py
@@ -6,3 +6,7 @@ class DirectoryView:
     def display_data(data: Dict[str, Any]) -> None:
         for path, info in data.items():
             print(f'{path}: {info["type"]} - {info["name"]} - {info["size"]} bytes')
+
+    @staticmethod
+    def display_message(message: str) -> None:
+        print(message)
